57c48366c5f8b9f0da287af065689113caab06cd
Sumeya <sumeyaibrahim72@gmail.com>
2023-07-21 18:51:37 +0300

    Fix linter errors

--- enumerable.rb
+++ enumerable.rb
@@ -1,17 +1,20 @@
+# frozen_string_literal: true
+
+# provides custom enumerable methods for arrays.
 module MyEnumerable
-    def all?(&block)
-      each { |e| return false unless block.call(e) }
-      true
-    end
-  
-    def any?(&block)
-      each { |e| return true if block.call(e) }
-      false
-    end
-  
-    def filter(&block)
-      result = []
-      each { |e| result << e if block.call(e) }
-      result
-    end
+  def all?(&block)
+    each { |e| return false unless block.call(e) }
+    true
   end
+
+  def any?(&block)
+    each { |e| return true if block.call(e) }
+    false
+  end
+
+  def filter(&block)
+    result = []
+    each { |e| result << e if block.call(e) }
+    result
+  end
+end
--- list.rb
+++ list.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 require_relative 'enumerable'
 
+# represents the list
 class MyList
   include MyEnumerable
 
@@ -9,17 +12,16 @@ class MyList
 
   def each(&block)
     @list.each(&block)
-  end
+  end
 end
 
-require_relative 'my_list'
 
 list = MyList.new(1, 2, 3, 4)
 
-puts list.all? { |e| e < 5 } #=> true
-puts list.all? { |e| e > 5 } #=> false
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
 
-puts list.any? { |e| e == 2 } #=> true
-puts list.any? { |e| e == 5 } #=> false
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
 
-puts list.filter { |e| e.even? } #=> [2, 4]
+puts(list.filter(&:even?))
