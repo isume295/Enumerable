bfc0d903149eef50a0d9b3a43e9f0b1f94603e91
Sumeya <sumeyaibrahim72@gmail.com>
2023-07-21 20:17:46 +0300

    fix linters

--- list.rb
+++ list.rb
@@ -1,4 +1,4 @@
- require_relative 'enumerable'
+require_relative 'enumerable'
 
 # represents the list
 class MyList
@@ -18,8 +18,6 @@ list = MyList.new(1, 2, 3, 4)
 
 puts(list.all? { |e| e < 5 })
 puts(list.all? { |e| e > 5 })
-
 puts(list.any? { |e| e == 2 })
 puts(list.any? { |e| e == 5 })
-
 puts(list.filter(&:even?))
